e5e03228e4af012d3acd7982d82759c8d5ace0f2
AIMANE ELBAKKALI <aimane.elb@gmail.com>
2019-06-20 21:42:28 +0100

    Shop category menu Added

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
 
-  constructor() { }
+
+export class NavbarComponent implements OnInit {
+  @ViewChild('shopCatgoryMenu', {static : false }) shopCatgoryMenu: ElementRef;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+
   }
 
+  openShopCatgoryMenu(){
+    this.renderer.setStyle(this.shopCatgoryMenu.nativeElement, 'height', '100%');   
+  }
+
+  closeShopCatgoryMenu(){
+    this.renderer.setStyle(this.shopCatgoryMenu.nativeElement, 'height', '0%');   
+  }
 }
