795d6df8b26660541126984e7da90ba367f87ecb
AIMANE ELBAKKALI <aimane.elb@gmail.com>
2019-06-26 00:33:56 +0100

    products grid added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SliderComponent } from './home/slider/slider.component';
 import { PresentationComponent } from './home/presentation/presentation.component';
 
 import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
+import { ProductsComponent } from './home/products/products.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
     HomeComponent,
     NavbarComponent,
     SliderComponent,
-    PresentationComponent
+    PresentationComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
